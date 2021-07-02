4a692ccc3610e67bde2a726f23f14b149fe2ee2a
bastinald <bastinald@icloud.com>
2021-07-02 01:49:03 -0400

    fix title

--- resources/stubs/ui/resources/views/layouts/app.blade.php
+++ resources/stubs/ui/resources/views/layouts/app.blade.php
@@ -5,10 +5,7 @@
     <meta name="viewport" content="width=device-width, initial-scale=1">
     <meta name="csrf-token" content="{{ csrf_token() }}">
 
-    <title>
-        @hasSection('title') @yield('title') | @endif
-        {{ config('app.name') }}
-    </title>
+    <title>@hasSection('title') @yield('title') | @endif {{ config('app.name') }}</title>
 
     <livewire:styles/>
     <link rel="stylesheet" href="{{ mix('css/app.css') }}">
