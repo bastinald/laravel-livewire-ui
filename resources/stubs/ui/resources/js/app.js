7f1470397223bfbb51dfdbe0714a49a6fbf404b4
bastinald <bastinald@icloud.com>
2021-07-02 02:30:09 -0400

    fix js paths

--- resources/stubs/ui/resources/js/app.js
+++ resources/stubs/ui/resources/js/app.js
@@ -1,4 +1,4 @@
 require('@popperjs/core');
 require('bootstrap');
-require('../../vendor/bastinald/laravel-livewire-loader/js/loader');
-require('../../vendor/bastinald/laravel-livewire-modals/js/modals');
+require('../../vendor/bastinald/laravel-livewire-loader/resources/js/loader');
+require('../../vendor/bastinald/laravel-livewire-modals/resources/js/modals');
